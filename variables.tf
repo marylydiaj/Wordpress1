b2dd87a80beae34bc7cbac690067bfaa1535f69d
marylydiaj <61267261+marylydiaj@users.noreply.github.com>
2020-05-11 22:32:17 -0700

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "active_key" {
+    default = "user"
+}
+variable "security_key" {
+    default = "password"
+}
+variable "aws_region" {
+    default = "us-east-1"
+}
+variable "privateec2_ip" {
+    default = "10.0.0.5"
+}
+variable "image" {
+    description = "instance images"
+    default     = "ami-0323c3dd2da7fb37d"
+}
+variable "instance_type" {
+    description = "instance type"
+    default     = "t2.micro"
+}
+variable "key" {
+    description = "instance key name"
+    default     = "godsnv"
+}
