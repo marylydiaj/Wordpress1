adb5ef04c51ede71e2cea52642e0a44f4672829b
marylydiaj <61267261+marylydiaj@users.noreply.github.com>
2020-05-12 13:00:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,10 +11,8 @@ sudo setenforce 0
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 sudo yum install php70w php70w-opcache php70w-mbstring php70w-gd php70w-xml php70w-pear php70w-fpm php70w-mysql php70w-pdo -y
-
 sudo yum -y install mariadb-server
 sudo systemctl start mariadb && sudo systemctl enable mariadb
-
 #MySQL Consider changing values from drupaldb, drupaluser, and password
 sudo echo "CREATE DATABASE zippyopsdb CHARACTER SET utf8 COLLATE utf8_general_ci;;" | mysql
 sudo echo "CREATE USER 'zippyops'@'localhost' IDENTIFIED BY 'zippyops';" | mysql
@@ -22,27 +20,35 @@ sudo echo "GRANT ALL PRIVILEGES ON zippyopsdb.* TO 'zippyops'@'localhost';" | my
 sudo echo "FLUSH PRIVILEGES;" | mysql
 cd /root/
 sudo git clone https://github.com/Ragu3492/wp-config.git
-
 sudo cd /var/www/html
-
 sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
 sudo php wp-cli.phar --info
 sudo chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 sudo wp --info --allow-root
-
-
 cd /var/www/html
 wp core download --allow-root
 #wp config create --dbname=zippyopsdb --dbuser=zippyops --dbpass=zippyops --locale=ro_RO --allow-root
 sudo cp /root/wp-config/wp-config.php /var/www/html/
 ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-wp core install --url=$ip --title=zippyops --admin_user=zippyops --admin_password=zippyops --admin_email=admin@zippyops.com --allow-root
+wp core install --url=$ip --title=zippyops --admin_user=zippyops --admin_password=zippyops --admin_email=admin@zippyops.com(opens in new tab) --allow-root
 sudo chown -R apache /var/www/html
 #wp theme install Consulting --allow-root
 #wp theme activate consulting --allow-root
 wp plugin install wordpress-importer --activate --allow-root
-
-sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
-sudo echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDlvu4zNd+Ng4K5twKW3iaxvxXXD4pZ8iehQ8h+fDoDdEQIjV6pTfmTmFdYY1Ilt54ETvpvGSZkM3aPbqBX1HEmt3sc/JF8EjdQ63L0phhGnjulLeUIGCydNANZSedTfmcQ+llbaFIrhYNiKMOwkAARj8Sb3E1Y6ZsoUCGekPkDw8s1OlJEhIudxKT3Y7SsvCuP8aWgposC4DGBbBIIq+UipqBI0l6kOFB+fp8hPDY3x4AnrpxqeAgXKpTPYGP53z3vJF25l2K4s3+53mfy+c5c2NcoGxbE0hB1E5fWyaBun9vpRwtwGYBcmH+s0dDIWxR5P+5TsgJl7eO+kSLvDdPp jenkins@jenkins-cloud.novalocal" > /home/ec2-user/.ssh/authorized_keys
+cd
+endpoint=`aws rds --region us-east-1 describe-db-instances --query "DBInstances[*].Endpoint.Address"`
+echo >file $endpoint
+sed -i 's/[][]//g' /root/file
+sed -i 's/"//g' /root/file
+sed -i 's/ //g' /root/file
+endpoint=$(<file)
+echo $endpoint
+git clone -b deploy --single-branch https://github.com/marylydiaj/zippyops_wordpress.git
+sudo cp /root/Wordpress/zippyopssite.wordpress.2020-04-22.000.xml /var/www/html/
+cd /var/www/html
+wp theme install Consulting --allow-root
+wp theme activate consulting --allow-root
+wp import zippyopssite.wordpress.2020-04-22.000.xml --authors=create --allow-root
+mysql -u zippyops -pzippyops -h $endpoint --database zippyops < wordpressdb.sql
+systemctl restart httpd
