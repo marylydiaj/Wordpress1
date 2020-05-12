63ac64ae3129b703fe3d720bd937c583daca5688
marylydiaj <61267261+marylydiaj@users.noreply.github.com>
2020-05-12 11:26:01 +0530

    Update lb2.tf

--- lb2.tf
+++ lb2.tf
@@ -99,7 +99,7 @@ resource "aws_lb" "alb" {
 
 resource "aws_lb_target_group" "alb_target_group" {  
   name     = "alb-target-group"  
-  port     = "8000"  
+  port     = "80"  
   protocol = "HTTP"  
   vpc_id   = aws_vpc.MyVPC.id
   tags = {    
