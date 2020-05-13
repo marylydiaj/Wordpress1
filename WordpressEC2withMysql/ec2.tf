87aa6bcfbda898b8389ec9b3b419c7d6936ccbb5
marylydiaj <61267261+marylydiaj@users.noreply.github.com>
2020-05-13 05:09:07 -0700

    Update ec2.tf

--- WordpressEC2withMysql/ec2.tf
+++ WordpressEC2withMysql/ec2.tf
@@ -1,108 +1,130 @@
-resource "aws_security_group" "websg" {
-    name = "vpc_web"
-    description = "Allow incoming HTTP connections."
-    vpc_id = aws_vpc.customvpc.id
+resource "aws_vpc" "tfvpc" {
+         cidr_block = "192.2.0.0/16"
+enable_dns_hostnames = "true"
+tags =  {
+         Name = "Terraform"
+     }
+                             }
 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        from_port = -1
-        to_port = -1
-        protocol = "icmp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-       from_port   = 8000
-       to_port     = 8000
-       protocol    = "tcp"
-       cidr_blocks = ["0.0.0.0/0"]
-  }
-   egress {
-       from_port   = 8000
-       to_port     = 8000
-       protocol    = "tcp"
-       cidr_blocks = ["0.0.0.0/0"]
-   }
-   ingress {
-       from_port   = 3306
-       to_port     = 3306
-       protocol    = "tcp"
-       cidr_blocks = ["0.0.0.0/0"]
-  }
-   egress {
-       from_port   = 3306
-       to_port     = 3306
-       protocol    = "tcp"
-       cidr_blocks = ["0.0.0.0/0"]
-   }
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+resource "aws_subnet" "tfpublicsubnet" {
+        vpc_id = aws_vpc.tfvpc.id
+        cidr_block = "192.2.2.0/24"
+        availability_zone = "us-east-1a"
+tags = {
+        Name = "TerraformSubnets"
+     }
+                                  }
 
-    tags = {
-        Name = "WebSG"
-        Terraform = true
-    }
-}
-
-resource "aws_instance" "web1" {
-    ami = var.image
-    availability_zone = "us-east-1a"
-    instance_type = var.instance_type
-    key_name = var.key
-    user_data = data.template_file.web1.rendered
-    vpc_security_group_ids = [aws_security_group.websg.id]
-    subnet_id = aws_subnet.us-east-1a-public.id
-    private_ip = var.publicec2_ip
-    associate_public_ip_address = true
-    source_dest_check = false
-    root_block_device {
+#############################################################################
+resource "aws_instance" "FirsttfInstance" {
+ami = var.image
+instance_type = var.instance_type
+subnet_id = aws_subnet.tfpublicsubnet.id
+private_ip = var.privateec2_ip
+key_name = var.key
+user_data = data.template_file.FirsttfInstance.rendered
+get_password_data = "false"
+availability_zone = "us-east-1a"
+security_groups = [aws_security_group.tfsecuritygroup.id]
+associate_public_ip_address = true
+root_block_device {
        volume_type           = "gp2"
        volume_size           = var.size
        delete_on_termination = "true"
-    }
-    tags = {
-        Name = "WebServer"
-        Terraform = true
-    }
-    provisioner "local-exec" {
-         command = "echo ${aws_instance.web1.public_ip} >> /var/lib/jenkins/workspace/DjangoMultiChoice/publicip"
-    }
-     provisioner "local-exec" {
-         command = "echo ${aws_instance.web1.private_ip} >> /var/lib/jenkins/workspace/DjangoMultiChoice/privateip"
-    }
 }
-data "template_file" "web1" {
+tags =  {
+       Name = "WordpressoInstance"
+     }
+provisioner "local-exec" {
+    command = "echo ${aws_instance.FirsttfInstance.public_ip} >> /var/lib/jenkins/workspace/WordpressMultiChoice/publicip"
+}
+}
+data "template_file" "FirsttfInstance" {
   template = file("install.sh")
 }
+##################################################################################
 
-#resource "aws_eip" "web1" {
-#    instance = aws_instance.web1.id
-#    vpc = true
-#}
-#resource "aws_network_interface" "web1" {
-#  subnet_id       = aws_subnet.us-east-1a-public.id
-#  private_ips     = ["10.0.0.11"]
-#  security_groups = [aws_security_group.websg.id]
-#}
+resource "aws_internet_gateway" "tfgw"{
+vpc_id = aws_vpc.tfvpc.id
 
+tags = {
+       Name = "tfgateway"
+     }
+}
+resource "aws_route_table_association" "tf" {
+subnet_id = aws_subnet.tfpublicsubnet.id
+route_table_id = aws_route_table.tf.id
+}
+resource "aws_route_table" "tf" {
+vpc_id = aws_vpc.tfvpc.id
 
+route{
+cidr_block = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.tfgw.id
+}
+
+tags = {
+       Name = "Publicroute"
+
+}
+}
+
+###############################################################################
+resource "aws_security_group" "tfsecuritygroup" {
+vpc_id = aws_vpc.tfvpc.id
+ingress {
+      protocol = "tcp"
+      self = true
+      from_port = 22
+      to_port = 22
+      cidr_blocks = ["0.0.0.0/0"]
+         }
+
+egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      }
+
+ingress {
+      protocol = "tcp"
+      self  = true
+      from_port = 80
+      to_port = 80 
+      cidr_blocks = ["0.0.0.0/0"]
+        }
+
+ingress {
+      protocol = "tcp"
+      self  = true
+      from_port = 8000
+      to_port = 8000
+      cidr_blocks = ["0.0.0.0/0"]
+        }
+egress {
+      protocol = "tcp"
+      self  = true
+      from_port = 8000
+      to_port = 8000
+      cidr_blocks = ["0.0.0.0/0"]
+        }
+ingress {
+      protocol = "tcp"
+      self  = true
+      from_port = 3306
+      to_port = 3306
+      cidr_blocks = ["0.0.0.0/0"]
+        }
+egress {
+      protocol = "tcp"
+      self  = true
+      from_port = 3306
+      to_port = 3306
+      cidr_blocks = ["0.0.0.0/0"]
+        }
+tags = {
+       Name = "tfsecuritygroup"
+     }
+
+}
